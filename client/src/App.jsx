54dd5e29b33c4b46acb56593dbb9167a2855b605
Grant Belcher <gtbelch@gmail.com>
2022-03-31 11:50:29 -0700

    tests css modules

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './App.module.css';
 import piazza from './piazza.jpeg';
+import Header from './components/Header/Header.jsx';
 
-import Test from './components/Test';
+
 
 function App() {
   return (
     <div>
-      <h1 className={styles.wow}>Hello from react!</h1>
-      <Test />
+      <Header />
       <img src={piazza} alt="" />
     </div>
   );
